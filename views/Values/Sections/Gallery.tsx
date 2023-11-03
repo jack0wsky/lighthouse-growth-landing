49757333d1368240d0108a288749c39beb449a6e
Jacek Łudzik <jacek.ludzik@elpassion.pl>
2023-11-03 15:05:47 +0100

    prioritize first images

--- views/Values/Sections/Gallery.tsx
+++ views/Values/Sections/Gallery.tsx
@@ -1,14 +1,21 @@
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper";
+import { images } from "@/views/Values/Sections/images";
 import "swiper/swiper.css";
 
 interface GalleryItemProps {
   path: string;
   objectPosition: string;
   alt: string;
+  priority: boolean;
 }
-const GalleryItem = ({ path, alt, objectPosition }: GalleryItemProps) => {
+const GalleryItem = ({
+  path,
+  alt,
+  objectPosition,
+  priority,
+}: GalleryItemProps) => {
   return (
     <div className="relative w-full h-[300px] sm:h-[350px]">
       <Image
@@ -16,56 +23,11 @@ const GalleryItem = ({ path, alt, objectPosition }: GalleryItemProps) => {
         style={{ objectFit: "cover", objectPosition }}
         fill
         alt={alt}
+        priority={priority}
       />
     </div>
   );
 };
-
-interface Image {
-  path: string;
-  position: "center" | "top";
-  alt: string;
-}
-const images: Image[] = [
-  { path: "22.webp", position: "center", alt: "" },
-  { path: "21.webp", position: "center", alt: "" },
-  { path: "20.webp", position: "center", alt: "" },
-  { path: "13.webp", position: "center", alt: "Mike and Jakub on IBC2023" },
-  { path: "14.webp", position: "center", alt: "Jakub on IBC2023" },
-  { path: "15.webp", position: "center", alt: "IBC2023 and badges" },
-  { path: "16.webp", position: "center", alt: "" },
-  { path: "17.webp", position: "center", alt: "Mike and Jakub" },
-  { path: "18.webp", position: "center", alt: "" },
-  { path: "19.webp", position: "center", alt: "" },
-  { path: "2.webp", position: "center", alt: "photo with couple pizzas" },
-  { path: "1.webp", position: "center", alt: "photo of our team" },
-  { path: "3.webp", position: "top", alt: "photo of Jakub and Gabi talking" },
-  { path: "4.webp", position: "center", alt: "photo from conference event" },
-  {
-    path: "5.webp",
-    position: "center",
-    alt: "chillout with two beers after work",
-  },
-  {
-    path: "6.webp",
-    position: "center",
-    alt: "badge on a conference",
-  },
-  { path: "7.webp", position: "center", alt: "view from plane" },
-  { path: "8.webp", position: "center", alt: "Jakub Szudejko - COO" },
-  {
-    path: "9.webp",
-    position: "center",
-    alt: "image from Las Vegas Convention Center",
-  },
-  {
-    path: "10.webp",
-    position: "center",
-    alt: "Mike and Jakub in Dubrovnik",
-  },
-  { path: "11.webp", position: "center", alt: "Jakub Szudejko - on a meeting" },
-  { path: "olivia.webp", position: "center", alt: "Olivia Business Center" },
-];
 
 export const Gallery = () => {
   return (
@@ -90,6 +52,7 @@ export const Gallery = () => {
               path={`/team-photos/${image.path}`}
               objectPosition={image.position}
               alt={image.alt}
+              priority={image.hasPriority}
             />
           </SwiperSlide>
         ))}
---
+++ views/Values/Sections/images.ts
@@ -0,0 +1,111 @@
+interface IImage {
+  path: string;
+  position: "center" | "top";
+  alt: string;
+  hasPriority: boolean;
+}
+
+export const images: IImage[] = [
+  { path: "22.webp", position: "center", alt: "", hasPriority: true },
+  { path: "21.webp", position: "center", alt: "", hasPriority: true },
+  { path: "20.webp", position: "center", alt: "", hasPriority: true },
+  {
+    path: "13.webp",
+    position: "center",
+    alt: "Mike and Jakub on IBC2023",
+    hasPriority: false,
+  },
+  {
+    path: "14.webp",
+    position: "center",
+    alt: "Jakub on IBC2023",
+    hasPriority: false,
+  },
+  {
+    hasPriority: false,
+    path: "15.webp",
+    position: "center",
+    alt: "IBC2023 and badges",
+  },
+  { hasPriority: false, path: "16.webp", position: "center", alt: "" },
+  {
+    hasPriority: false,
+    path: "17.webp",
+    position: "center",
+    alt: "Mike and Jakub",
+  },
+  { hasPriority: false, path: "18.webp", position: "center", alt: "" },
+  { hasPriority: false, path: "19.webp", position: "center", alt: "" },
+  {
+    hasPriority: false,
+    path: "2.webp",
+    position: "center",
+    alt: "photo with couple pizzas",
+  },
+  {
+    hasPriority: false,
+    path: "1.webp",
+    position: "center",
+    alt: "photo of our team",
+  },
+  {
+    hasPriority: false,
+    path: "3.webp",
+    position: "top",
+    alt: "photo of Jakub and Gabi talking",
+  },
+  {
+    hasPriority: false,
+    path: "4.webp",
+    position: "center",
+    alt: "photo from conference event",
+  },
+  {
+    hasPriority: false,
+    path: "5.webp",
+    position: "center",
+    alt: "chillout with two beers after work",
+  },
+  {
+    hasPriority: false,
+    path: "6.webp",
+    position: "center",
+    alt: "badge on a conference",
+  },
+  {
+    hasPriority: false,
+    path: "7.webp",
+    position: "center",
+    alt: "view from plane",
+  },
+  {
+    hasPriority: false,
+    path: "8.webp",
+    position: "center",
+    alt: "Jakub Szudejko - COO",
+  },
+  {
+    hasPriority: false,
+    path: "9.webp",
+    position: "center",
+    alt: "image from Las Vegas Convention Center",
+  },
+  {
+    hasPriority: false,
+    path: "10.webp",
+    position: "center",
+    alt: "Mike and Jakub in Dubrovnik",
+  },
+  {
+    hasPriority: false,
+    path: "11.webp",
+    position: "center",
+    alt: "Jakub Szudejko - on a meeting",
+  },
+  {
+    hasPriority: false,
+    path: "olivia.webp",
+    position: "center",
+    alt: "Olivia Business Center",
+  },
+];
