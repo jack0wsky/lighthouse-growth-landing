7cff0dcad41eabde3c3935ef1b71104ac7a4900c
Jacek Łudzik <jacek.ludzik@elpassion.pl>
2024-01-14 12:47:06 +0100

    fix hero height

--- views/Services/Electricians/Sections/Hero.tsx
+++ views/Services/Electricians/Sections/Hero.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 export const Hero = () => {
   const { heading } = useServicesDictionary();
   return (
-    <section className="h-[80vh] py-20 flex flex-col items-center relative">
+    <section className="h-[80vh] md:h-[55vh] py-20 flex flex-col items-center relative">
       <h1 className="text-black text-2xl md:text-3xl font-bold text-center w-5/6 md:w-2/3 mb-10 mt-24">
         {heading}
       </h1>
