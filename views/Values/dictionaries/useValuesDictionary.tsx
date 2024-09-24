0a32e8b94b06318461ba08b41ac6608c3ec74c03
Kamil Dzieniszewski <kamil.dzieniszewski@gmail.com>
2024-09-24 15:29:20 +0200

    feat: Values languages

--- views/Values/dictionaries/useValuesDictionary.tsx
+++ views/Values/dictionaries/useValuesDictionary.tsx
@@ -1,13 +1,13 @@
 import { Languages } from "@/shared/dictionaries/languages";
 import en from "./en.json";
 import de from "./de.json";
-// import lt from "./lt.json";
-// import lv from "./lv.json";
-// import ee from "./ee.json";
-// import fi from "./fi.json";
-// import se from "./se.json";
-// import no from "./no.json";
-// import is from "./is.json";
+import lt from "./lt.json";
+import lv from "./lv.json";
+import ee from "./ee.json";
+import fi from "./fi.json";
+import se from "./se.json";
+import no from "./no.json";
+import is from "./is.json";
 import { useParams } from "next/navigation";
 
 interface DictionaryModel {
@@ -33,12 +33,12 @@ const dictionaries: Record<Languages, DictionaryModel> = {
   en,
   de,
   lt,
-  // lv,
-  // ee,
-  // fi,
-  // se,
-  // no,
-  // is,
+  lv,
+  ee,
+  fi,
+  se,
+  no,
+  is,
 };
 
 export const useValuesDictionary = () => {
